c4f2f08b674d3c4fa003e8c7183c694a14757f6f
puja khadka <khadkapuja195@gmail.com>
2025-12-13 10:10:55 +0545

    change code

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -11,15 +11,17 @@ function App() {
     setCount(count)
   }
   const removevalue=()=>{
-    count=count-1
+   if(count>0){
+     count=count-1
     setCount(count)
+   }
   }
  return (
     <>
     <div style={{ display:"block",textAlign:"center",marginTop:"200px",boxShadow:"5px 5px 30px grey ",width:300,height:200,marginLeft:"500px",borderRadius:"10px",paddingTop:"30px",backgroundColor:"lightblue"}}>
    <h1 style={{color:"black" }}>Number:{count}</h1>
    <div style={{display:"flex",gap:"10px",justifyContent:"center"}}>
-   <button  onClick={addvalue}>click me</button>
+   <button  onClick={addvalue}>click me{count}</button>
    <button onClick={removevalue}>click me</button>
    </div>
    </div>
