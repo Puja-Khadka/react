f5d64ec7c7a5873f15d74efe04e0fe8a00a480fb
puja khadka <khadkapuja195@gmail.com>
2025-12-14 15:15:19 +0545

    using tailwind

---
+++ project2/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./Components/card.jsx";
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-amber-950 text-white inline-block m-5">Tailwind test</h1>
+     <Card/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ project2/src/Components/card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function card() {
+  return (
+   <div className="flex flex-col items-center p-7 rounded-2xl shadow-black shadow-lg w-64 gap-4 m-auto">
+  <div>
+    <img className="size-48 shadow-xl rounded-md" alt src="https://images.pexels.com/photos/29707980/pexels-photo-29707980.jpeg?cs=srgb&dl=pexels-m-emre_celik-2054744248-29707980.jpg&fm=jpg&_gl=1*1d82rg4*_ga*NDkwMjIyNzQ1LjE3NjU3MDM2NDU.*_ga_8JE65Q40S6*czE3NjU3MDM2NDQkbzEkZzEkdDE3NjU3MDM2NjYkajM4JGwwJGgw" />
+  </div>
+  <div className="">
+    <span className="font-bold">Class Warfare</span><br/>
+    <span>The Anti-Patterns</span>
+    <span className="flex">
+      <span >No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
+  )
+}
