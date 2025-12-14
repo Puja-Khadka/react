c7c927a263ef0f30adfca781a1526b30c953f940
puja khadka <khadkapuja195@gmail.com>
2025-12-14 16:43:37 +0545

    update props

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
       <h1 className="bg-amber-950 text-white inline-block m-5">Tailwind test</h1>
-     <Card username="Cat"/>
-     <Card/>
+     <Card username="Cat" />
+     <Card username="beautiful cat"/>
     
     </>
   )
