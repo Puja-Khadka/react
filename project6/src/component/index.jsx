bdbc81c377894a445c8cbc3e73d4bc054a0b4c42
puja khadka <khadkapuja195@gmail.com>
2025-12-19 15:35:17 +0545

    experiment

--- project6/src/component/index.jsx
+++ project6/src/component/index.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import useTogglehook from './Togglehook'
 
 function Index() {
-    const [isVisible,toggleVisibility]=useTogglehook(false)
+    //const [isVisible,toggleVisibility]=useTogglehook(false)
+    const [value,toggle]=useTogglehook(false)
   return (
     <div>
-      <button className='border mb-12 mt-5' onClick={toggleVisibility}>{isVisible?"hide text":"show text"}</button>
-      {isVisible && <p className='mb-10'> this is paragraph</p>}
+      <button className='border mb-12 mt-5' onClick={toggle}>{value?"hide text":"show text"}</button>
+      {value && <p className='mb-10'> this is paragraph</p>}
     </div>
   )
 }
