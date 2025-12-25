f3a610395afa04be2363f4199f812003960fd694
puja khadka <khadkapuja195@gmail.com>
2025-12-25 17:05:42 +0545

    another example using useMemo

--- project10/src/App.jsx
+++ project10/src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useCallback } from 'react'
+import Expensivetask from './Component/expensivetask';
 
 const Button=React.memo(({onClick,text})=>{
 console.log(`Child ${text} render`);
@@ -8,9 +9,15 @@ return <button onClick={onClick}>{text}</button>
 })
 
 function App() {
+  
   const [count1, setCount1] = useState(0)
   const [count2, setCount2] = useState(0)
+  const [count3,setCount3]=useState(0)
+  const [input,setInput]=useState(0)
  
+ const doublevalue= useMemo(()=>{
+return Expensivetask(input)
+  },[input])
 
   const handleClick1=useCallback(()=>{
  
@@ -29,8 +36,14 @@ function App() {
     <h2>Without useCallback:</h2>
       <p>Count 1: {count1}</p>
       <p>Count 2: {count2}</p>
-     <Button  onClick={handleClick1} text="Button1"/> 
-     <Button  onClick={handleClick2} text="Button2"/> 
+    {/* <Button  onClick={handleClick1} text="Button1"/>  */}
+     {/* <Button  onClick={handleClick2} text="Button2"/>  */}
+     <p>count3:{count3}</p>
+    <button onClick={()=>setCount3(count3+1)}>increment</button>
+    <input type="number" placeholder='enter a number'value={input} onChange={(e)=>setInput(e.target.value)}/>
+    <p>number is:{doublevalue}</p>
+  
+
     </>
   )
 }
---
+++ project10/src/Component/Expensivetask.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Expensivetask(num) {
+    console.log("calculating...");
+    
+    for(let i=0;i<=1000000000;i++){}
+ return num*2
+}
+
+export default Expensivetask
